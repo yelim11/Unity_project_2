978c8adb913673e75ebf842197bcea4f26e8563b
yelim <oyelim00@naver.com>
2023-11-12 16:23:59 +0900

    2023.11.12 연습

---
+++ Assets/step2.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class step2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string myString = "æ»≥Á«œººø‰!";
+
+        Debug.Log(myString);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
